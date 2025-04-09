43c4bebc9ae70ec3bcc924c553d8db8bdc4bbf89
max0mars <maxmarsyyz@icloud.com>
2025-04-09 17:42:44 -0400

    more cleanup

--- main.lua
+++ main.lua
@@ -1,12 +1,11 @@
 require('building')
-require('entity')
 require('minebot')
 require('factory')
-require('defensebot')
-require('enemybot')
+require('Enemy')
 require('bullet')
 require('button')
-require('EnemyBase')
+
+local scaling = require('scaling')
 --io.stdout:setvbuf("no") May or may not be needed for print statements
 
 function love.load()
@@ -20,66 +19,15 @@ function love.load()
 end
 
 function love.update(dt)
-    if(pause) then return end
-    if(factory.health <= 0) then GameState = 2 end
-    if(GameState == 0) then
-        for i in ipairs(Enemies) do
-            Enemies[i]:update(dt, defensebots, factory)
-        end
-        for i in ipairs(minebots) do
-            minebots[i]:update(dt)
-        end
-        for i in ipairs(bullets) do
-            bullets[i]:update(dt)
-        end
-        waveupdate(dt)
-        keydown(dt) -- keyboard input for holding down a button
-        CleanTable(defensebots)
-        CleanTable(Enemies)
-        CleanTable(bullets)   
-    end 
 end
 
 function love.draw()
-    if pause then love.graphics.print("Paused", 400, 150) end
-    if GameState == 1 then
-        love.graphics.print("You Win!", 400, 300)
-        try_again:draw()
-    elseif GameState == 2 then
-        local txt = "You Lose!\n" .. "Better Luck Next Time!"
-        love.graphics.print(txt, 400 - font:getWidth(txt)/2, 300 - font:getHeight(txt)/2)
-        try_again:draw()
-    elseif(GameState == 0) then 
-        --if upgradeMenu then
-            
-        mine:draw()
-        factory:draw()
-        enemybase:draw()
-        for i in ipairs(Enemies) do
-            Enemies[i]:draw()
-        end
-        for i in ipairs(minebots) do
-            minebots[i]:draw()
-        end
-        for i in ipairs(bullets) do
-            bullets[i]:draw()
-        end
-        for i in ipairs(buttons) do
-            buttons[i]:draw()
-        end
-        love.graphics.setColor(0,1,0)
-        love.graphics.line(0, 200, 800, 200)
-        love.graphics.line(0, 500, 800, 500)
-    end
-    
 
 end
 
 function love.keypressed(key, scancode, isrepeat)
     if key == "escape" then
        love.event.quit()
-    elseif key == "p" then
-        pause = not pause
     end
 end
 
@@ -96,28 +44,6 @@ function love.mousepressed(x, y, button, istouch, presses)
     end
 end
 
-function waveupdate(dt)
-    counter2 = counter2 - dt
-    if(counter2 < 0) then
-        print("wave " .. wavecount)
-        spawnsremaining = wavecount * wavecount * 0.5
-        wavecount = wavecount + 1
-        counter2 = waverate
-    end
-
-    counter = counter - dt
-        if(counter < 0 and spawnsremaining > 0) then
-            table.insert(enemybots, enemybot:new(bullets))
-            spawnsremaining = spawnsremaining - 1
-            counter = spawnrate
-        end
-end
-
-function keydown(dt)
-    
-end
-
-
 function CleanTable(t) -- dereferences any elements marked for deletion
     local j = 1
     n = #t
@@ -132,8 +58,4 @@ function CleanTable(t) -- dereferences any elements marked for deletion
             j = j + 1
         end
     end
-end
-
-function init()
-
 end
---
+++ scaling.lua
@@ -0,0 +1,53 @@
+local scaling = {}
+
+-- Default values for the drawing area
+scaling.DRAW_WIDTH = 512
+scaling.DRAW_HEIGHT = 448
+scaling.PIXEL_SCALE = 1
+scaling.DRAW_SCALE = 1
+scaling.LEFT_OFFSET = 0
+scaling.TOP_OFFSET = 0
+
+-- Initialize the scaling system
+function scaling.init(drawWidth, drawHeight)
+    scaling.DRAW_WIDTH = drawWidth or scaling.DRAW_WIDTH
+    scaling.DRAW_HEIGHT = drawHeight or scaling.DRAW_HEIGHT
+    scaling.recalculate()
+end
+
+-- Recalculate scaling and offsets based on window size
+function scaling.recalculate()
+    local w, h = love.graphics.getWidth(), love.graphics.getHeight()
+
+    -- Calculate scaling factors
+    local scaleHorizontal = w / scaling.DRAW_WIDTH
+    local scaleVertical = h / scaling.DRAW_HEIGHT
+
+    -- Determine the appropriate scale to maintain aspect ratio
+    if scaling.DRAW_WIDTH / scaling.DRAW_HEIGHT <= w / h then
+        scaling.DRAW_SCALE = scaleVertical
+    else
+        scaling.DRAW_SCALE = scaleHorizontal
+    end
+
+    -- Calculate offsets for centering the drawing area
+    scaling.LEFT_OFFSET = (w - (scaling.DRAW_WIDTH * scaling.DRAW_SCALE)) / 2
+    scaling.TOP_OFFSET = (h - (scaling.DRAW_HEIGHT * scaling.DRAW_SCALE)) / 2
+
+    -- Handle HighDPI scaling
+    scaling.PIXEL_SCALE = love.window.getDPIScale()
+end
+
+-- Apply transformations for drawing
+function scaling.applyTransform()
+    love.graphics.push()
+    love.graphics.translate(scaling.LEFT_OFFSET, scaling.TOP_OFFSET)
+    love.graphics.scale(scaling.DRAW_SCALE)
+end
+
+-- Reset transformations after drawing
+function scaling.resetTransform()
+    love.graphics.pop()
+end
+
+return scaling
